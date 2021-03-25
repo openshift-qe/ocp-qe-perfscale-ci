2c236c555c28dc3026272f3396b93ed5958174e6
Kedar Vijay Kulkarni <kkulkarni@redhat.com>
2021-03-25 16:37:59 -0400

    Adding EUS upgrade type to jenkinsfile
    Signed-off-by: Kedar Vijay Kulkarni <kkulkarni@redhat.com>

--- upgrade_scripts/check_upgrade.py
+++ upgrade_scripts/check_upgrade.py
@@ -4,7 +4,7 @@ import time
 import yaml
 import subprocess
 import sys
-
+import json
 
 # Invokes a given command and returns the stdout
 def invoke(command):
@@ -15,13 +15,91 @@ def invoke(command):
         return exc.returncode, exc.output
     return 0, output
 
-
 def set_max_unavailable(maxUnavailable):
 
     merge_json = '{"spec":{"maxUnavailable": %d }}' % maxUnavailable
     return_code, output = invoke(f"oc patch machineconfigpool/worker --type='merge' -p='{merge_json}'")
     if return_code != 0:
         print("Error occurred trying to set maxUnavialble nodes")
+
+def get_sha_url(url,version):
+    return_code, output = invoke("curl " + url)
+    json_output = json.loads(output)
+    for version_json in json_output['nodes']:
+        if version_json['version'] == version:
+            print(version_json['payload'])
+            return
+    print("")
+
+def set_upstream_channel(version):
+
+    small_version = ".".join(version.split('.', 2)[:2])
+    print('small v ' + str(small_version))
+    if small_version == "4.9":
+        patch_48_to_49()
+    merge_json = '{"spec":{"channel": "fast-%s" }}' % small_version
+    return_code, output = invoke(f"oc patch clusterversion/version --type='merge' -p='{merge_json}'")
+    if return_code != 0:
+        print("Error occurred trying to patch channel version")
+    print('output ' + str(output))
+    return_code, output = invoke("oc get clusterversion -ojson|jq .items[].spec")
+    print('output ' + str(output))
+
+def patch_48_to_49():
+    merge_json = '{"data":{"ack-4.8-kube-1.22-api-removals-in-4.9":"true"}}'
+    print('patch api')
+    return_code, output = invoke(f"oc -n openshift-config patch cm admin-acks --type=merge -p='{merge_json}'")
+    print('output: ' + str(output))
+
+def set_upstream_url(url):
+
+    merge_json = '{"spec":{"upstream": "%s" }}' % url
+    return_code, output = invoke(f"oc patch clusterversion/version --type='merge' -p='{merge_json}'")
+    if return_code != 0:
+        print("Error occurred trying to patch channel url")
+    print('output ' + str(output))
+
+def verify_version_in_channel_list(expected_version):
+    return_code, output = invoke("oc adm upgrade")
+    if return_code != 0:
+        print("ERROR")
+
+    split_output = output.split("Recommended updates")
+    new_line_output = split_output[-1].split('\n')
+    for line in new_line_output:
+        space_line = line.split('   ')[0]
+        if expected_version == space_line.strip():
+            print('expected version in list')
+            return
+    print("ERROR")
+
+def get_upgrade_status(mcp_json):
+    for mcp_status in mcp_json['status']['conditions']:
+        if mcp_status['type'] == "Updating":
+            return mcp_status['status']
+
+def wait_for_mcp_upgrade(wait_num=90):
+    counter = 0
+    return_code, worker_str = invoke(f"oc get mcp worker -o json")
+    worker_json = json.loads(worker_str)
+    while get_upgrade_status(worker_json) != "False":
+        time.sleep(20)
+        return_code, worker_str = invoke(f"oc get mcp worker -o json")
+        worker_json = json.loads(worker_str)
+        print("MCP workers are updating, waiting 20 seconds")
+        counter += 1
+        if counter >= wait_num:
+            return_code, node_not_ready = invoke("oc get nodes | grep 'NotReady\|SchedulingDisabled'")
+            print("Node list: " + str(node_not_ready))
+            print("ERROR, mcp workers are still updating after 30 minutes")
+            sys.exit(1)
+
+def pause_machinepool_worker(pause_val):
+    merge_json = '{"spec":{"paused": %s }}' % pause_val
+    return_code, output = invoke(f"oc patch --type=merge --patch='{merge_json}' machineconfigpool/worker")
+    if return_code != 0:
+        print("Error occurred trying to pause machineconfigpool/worker")
+    print('output ' + str(output))
 
 def check_cluster_version():
 
