2960b1479e333ccd9035601445cb0ce2a9c82346
memodi <memodi@redhat.com>
2022-04-13 16:02:56 -0400

    call export_defaults network-perf prior

--- scripts/run_workloads.sh
+++ scripts/run_workloads.sh
@@ -8,14 +8,15 @@ case ${WORKLOAD_TYPE} in
 uperf)
     export NETWORK_PERF_DIR='../../e2e-benchmarking/workloads/network-perf'
     source $NETWORK_PERF_DIR/env.sh
+    source $NETWORK_PERF_DIR/common.sh
     override_uperf_env
     prep_uperf_workload
-    cd $NETWORK_PERF_DIR && WORKLOAD="pod2pod" ./run.sh && cd -
+    cd $NETWORK_PERF_DIR && export WORKLOAD="pod2pod" ./run.sh && cd -
     ;;
 node-density-heavy)
     export KUBE_BURNER_DIR='../../e2e-benchmarking/workloads/kube-burner'
     #TODO: update
-    cd $KUBE_BURNER_DIR && WORKLOAD="node-density-heavy" ./run.sh && cd -
+    cd $KUBE_BURNER_DIR && export WORKLOAD="node-density-heavy" ./run.sh && cd -
     ;;
 
 esac
