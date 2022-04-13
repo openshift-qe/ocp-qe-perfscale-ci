c96f66a9da41e8b050cc6931ac2db9853df38e42
memodi <memodi@redhat.com>
2022-04-13 16:42:17 -0400

    add workload type to Jenkinsfile and more params

--- scripts/run_workloads.sh
+++ scripts/run_workloads.sh
@@ -1,4 +1,5 @@
 source ../../e2e-benchmarking/utils/common.sh
+source ../../e2e-benchmarking/utils/benchmark-operator.sh
 source ./common.sh
 source ./netobserv.sh
 
@@ -11,12 +12,14 @@ uperf)
     source $NETWORK_PERF_DIR/common.sh
     override_uperf_env
     prep_uperf_workload
-    cd $NETWORK_PERF_DIR && export WORKLOAD="pod2pod" ./run.sh && cd -
+    export WORKLOAD="pod2pod"
+    cd $NETWORK_PERF_DIR && ./run.sh && cd -
     ;;
 node-density-heavy)
     export KUBE_BURNER_DIR='../../e2e-benchmarking/workloads/kube-burner'
     #TODO: update
-    cd $KUBE_BURNER_DIR && export WORKLOAD="node-density-heavy" ./run.sh && cd -
+    export WORKLOAD="node-density-heavy"
+    cd $KUBE_BURNER_DIR && ./run.sh && cd -
     ;;
 
 esac
