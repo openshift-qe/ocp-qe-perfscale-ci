651ae7de9ecef0f7b45091414133fe241a251f80
memodi <memodi@redhat.com>
2022-04-19 12:08:35 -0400

    print uperf CRD

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,5 +1,6 @@
 prep_uperf_workload() {
     envsubst <ripsaw-uperf-crd.yaml >$NETWORK_PERF_DIR/ripsaw-uperf-crd.yaml
+    cat $NETWORK_PERF_DIR/ripsaw-uperf-crd.yaml
 }
 override_uperf_env() {
     export PAIRS=4
