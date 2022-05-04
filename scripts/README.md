7d77c87e3aa97d6e3d6a9e5ebd2c15719a63112b
Nathan Weinberg <31703736+nathan-weinberg@users.noreply.github.com>
2022-05-04 12:37:32 -0700

    Initial commit of NOPE tool (#135)
    * Initial commit of NOPE tool
    
    * Changes from PR feedback
    
    * Additional changes
    
    * Updated README

--- scripts/README.md
+++ scripts/README.md
@@ -5,3 +5,13 @@ Multiple workloads are run to generate traffic for the the cluster:
 1. uperf - pod-to-pod traffic generation.
 2. node-density-heavy (using kube-burner)
 3. router-perf
+
+## Network Observability Prometheus and Elasticsearch tool (NOPE)
+The Network Observability Prometheus and Elasticsearch tool, or NOPE, is a Python program that is used for collecting and sharing performance data for a given OpenShift cluster running the Network Observability Operator, using Prometheus queries for collection and Elasticsearch servers for sharing. Raw JSON files are written to the `data/` directory in the project - note this directory will be created automatically if it does not already exist.
+
+### Running the tool
+1. Ensure you have Python 3.9+ and Pip installed (verify with `python --version` and `pip --version`)
+2. Install requirements with `pip install -r scripts/requirements.txt`
+3. Run the tool with `./scripts/nope.py` with the `--url` arg being your cluster's URL and the `--token` arg being your token for Bearer authentication.
+    - You can get the cluster URL by running `oc get infrastructure cluster -o jsonpath="{.status.apiServerURL}"`
+    - You can get the token by logging into your cluster as `kubeadmin` and running `oc whoami -t`.
---
+++ scripts/nope.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+import pathlib
+import argparse
+import requests
+
+
+CLUSTER_URL = ''
+TOKEN = ''
+NAMESPACE = ''
+CPU_QUERY = ''
+MEM_QUERY = ''
+LOKI_QUERY = ''
+
+
+def run_query(query_type=''):
+
+	# intialize request vars
+	headers = {"Authorization": f"Bearer {TOKEN}"}
+	params = {}
+	endpoint = f"https://prometheus-k8s-openshift-monitoring.apps.{CLUSTER_URL}/api/v1/query"
+
+	# build payload based on query type
+	if query_type == "cpu":
+		params = {
+			'query': CPU_QUERY
+		}
+	elif query_type == "mem":
+		params = {
+			'query': MEM_QUERY
+		}
+	elif query_type == "loki":
+		params = {
+			'query': LOKI_QUERY
+		}
+	else:
+		print(f"invalid query_type: {query_type}")
+		sys.exit(1)
+	
+	# make request and return data
+	data = requests.get(endpoint, headers=headers, params=params, verify=False)
+	return data.json()
+
+
+def get_cpu_usage():
+	cpu_data = run_query('cpu')
+	return cpu_data
+
+
+def get_mem_usage():
+	mem_data = run_query('mem')
+	return mem_data
+
+
+def get_loki_store():
+	loki_data = run_query('loki')
+	return loki_data
+
+
+def main():
+
+	# get prometheus data
+	cpu_data = get_cpu_usage()
+	mem_data = get_mem_usage()
+	loki_data = get_loki_store()
+
+	# ensure data directory exists (create if not)
+	data_dir = str(pathlib.Path(__file__).parent.parent) + '/data'
+	pathlib.Path(data_dir).mkdir(parents=True, exist_ok=True)
+
+	# write prometheus data to data directory
+	with open(data_dir + '/cpu_data.json', 'w') as cpu_file:
+		json.dump(cpu_data, cpu_file)
+
+	with open(data_dir + '/mem_data.json', 'w') as mem_file:
+		json.dump(mem_data, mem_file)
+
+	with open(data_dir + '/loki_data.json', 'w') as loki_file:
+		json.dump(loki_data, loki_file)
+
+	# exit if no issues
+	print(f"Data captured successfully and written to {data_dir}")
+	sys.exit(0)
+
+
+if __name__ == '__main__':
+
+	# initialize argument parser
+	parser = argparse.ArgumentParser(description='Network Observability Prometheus and Elasticsearch tool (NOPE)')
+
+	# set customization flags
+	parser.add_argument("--url", type=str, help='Cluster URL', required=True)
+	parser.add_argument("--token", type=str, help='Token for Bearer auth', required=True)
+	parser.add_argument("--namespace", type=str, default='network-observability', help='Namespace for query - defaults to "network-observability"', required=False)
+
+	# parse arguments
+	args = parser.parse_args()
+	CLUSTER_URL = args.url
+	TOKEN = args.token
+	NAMESPACE = args.namespace
+
+	# clean arguments
+	CLUSTER_URL = CLUSTER_URL.removeprefix('https://api.')
+	CLUSTER_URL = CLUSTER_URL.removesuffix(':6443')
+
+	# set query constants
+	CPU_QUERY = f'pod:container_cpu_usage:sum{{namespace="{NAMESPACE}"}}'
+	MEM_QUERY = f'sum(container_memory_working_set_bytes{{namespace="{NAMESPACE}",container="",}}) BY (pod)'
+	LOKI_QUERY = f'kubelet_volume_stats_used_bytes{{namespace="{NAMESPACE}", persistentvolumeclaim="loki-store"}}'
+
+	# begin main program execution
+	main()
