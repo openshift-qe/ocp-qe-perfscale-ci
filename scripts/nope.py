aad66941d1cc9950daa755955a96901f4dd3c89d
Nathan Weinberg <nweinber@redhat.com>
2022-05-05 12:00:11 -0700

    Get Prometheus queries for NOPE from YAML file

--- scripts/nope.py
+++ scripts/nope.py
@@ -2,87 +2,58 @@
 
 import sys
 import json
+import yaml
 import pathlib
 import subprocess
 import argparse
 import requests
 
 
+# directory constants 
+ROOT_DIR = str(pathlib.Path(__file__).parent.parent)
+SCRIPT_DIR = ROOT_DIR + '/scripts'
+DATA_DIR = ROOT_DIR + '/data'
+
+# NOPE constants
 CLUSTER_URL = ''
 TOKEN = ''
-NAMESPACE = ''
-CPU_QUERY = ''
-MEM_QUERY = ''
-LOKI_QUERY = ''
+YAML_FILE = ''
+QUERIES = {}
+RESULTS = {}
 
 
-def run_query(query_type=''):
+def run_query(query):
 
 	# intialize request vars
-	headers = {"Authorization": f"Bearer {TOKEN}"}
-	params = {}
 	endpoint = f"https://prometheus-k8s-openshift-monitoring.apps.{CLUSTER_URL}/api/v1/query"
+	headers = {"Authorization": f"Bearer {TOKEN}"}
+	params = {
+		'query': query
+	}
 
-	# build payload based on query type
-	if query_type == "cpu":
-		params = {
-			'query': CPU_QUERY
-		}
-	elif query_type == "mem":
-		params = {
-			'query': MEM_QUERY
-		}
-	elif query_type == "loki":
-		params = {
-			'query': LOKI_QUERY
-		}
-	else:
-		print(f"invalid query_type: {query_type}")
-		sys.exit(1)
-	
 	# make request and return data
 	data = requests.get(endpoint, headers=headers, params=params, verify=False)
 	return data.json()
 
 
-def get_cpu_usage():
-	cpu_data = run_query('cpu')
-	return cpu_data
-
-
-def get_mem_usage():
-	mem_data = run_query('mem')
-	return mem_data
-
-
-def get_loki_store():
-	loki_data = run_query('loki')
-	return loki_data
-
-
 def main():
 
 	# get prometheus data
-	cpu_data = get_cpu_usage()
-	mem_data = get_mem_usage()
-	loki_data = get_loki_store()
+	num_queries = range(len(QUERIES))
+	for index in num_queries:
+		metric_name = QUERIES[index]['metricName']
+		query = QUERIES[index]['query']
+		RESULTS[metric_name] = run_query(query)
 
 	# ensure data directory exists (create if not)
-	data_dir = str(pathlib.Path(__file__).parent.parent) + '/data'
-	pathlib.Path(data_dir).mkdir(parents=True, exist_ok=True)
+	pathlib.Path(DATA_DIR).mkdir(parents=True, exist_ok=True)
 
 	# write prometheus data to data directory
-	with open(data_dir + '/cpu_data.json', 'w') as cpu_file:
-		json.dump(cpu_data, cpu_file)
-
-	with open(data_dir + '/mem_data.json', 'w') as mem_file:
-		json.dump(mem_data, mem_file)
-
-	with open(data_dir + '/loki_data.json', 'w') as loki_file:
-		json.dump(loki_data, loki_file)
+	with open(DATA_DIR + '/data.json', 'w') as data_file:
+		json.dump(RESULTS, data_file)
 
 	# exit if no issues
-	print(f"Data captured successfully and written to {data_dir}")
+	print(f"Data captured successfully and written to {DATA_DIR}")
 	sys.exit(0)
 
 
@@ -94,7 +65,7 @@ if __name__ == '__main__':
 	# set customization flags
 	parser.add_argument("--url", type=str, help='Override for Cluster URL', required=False)
 	parser.add_argument("--token", type=str, help='Override for Bearer auth token', required=False)
-	parser.add_argument("--namespace", type=str, default='network-observability', help='Namespace for query - defaults to "network-observability"', required=False)
+	parser.add_argument("--yaml_file", type=str, default='netobserv-metrics.yaml', help='YAML file from which to source Prometheus queries - defaults to "netobserv-metrics.yaml"', required=False)
 
 	# parse arguments
 	args = parser.parse_args()
@@ -109,8 +80,8 @@ if __name__ == '__main__':
 		TOKEN = subprocess.run(['oc', 'whoami', '-t'], capture_output=True, text=True).stdout
 		print(f"TOKEN (raw): {TOKEN}")
 
-	NAMESPACE = args.namespace
-	print(f"NAMESPACE: {NAMESPACE}")
+	YAML_FILE = args.yaml_file
+	print(f"YAML_FILE: {YAML_FILE}")
 
 	# clean arguments
 	CLUSTER_URL = CLUSTER_URL.removeprefix('"https://api.')
@@ -120,10 +91,13 @@ if __name__ == '__main__':
 	TOKEN = TOKEN[:-1]
 	print(f"TOKEN (cleaned): {TOKEN}")
 
-	# set query constants
-	CPU_QUERY = f'pod:container_cpu_usage:sum{{namespace="{NAMESPACE}"}}'
-	MEM_QUERY = f'sum(container_memory_working_set_bytes{{namespace="{NAMESPACE}",container="",}}) BY (pod)'
-	LOKI_QUERY = f'kubelet_volume_stats_used_bytes{{namespace="{NAMESPACE}", persistentvolumeclaim="loki-store"}}'
+	# set queries
+	with open(SCRIPT_DIR + '/' + YAML_FILE, 'r') as yaml_file:
+		try:
+			QUERIES = yaml.safe_load(yaml_file)
+		except Exception as e:
+			print(f'Failed to read YAML file {YAML_FILE}: {e}')
+			sys.exit(1)
 
 	# begin main program execution
 	main()
