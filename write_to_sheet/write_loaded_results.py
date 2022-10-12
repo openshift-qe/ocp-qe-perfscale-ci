c0df78f464270a5634a44e0020d1356c6e7f2c55
Paige Rubendall <64206430+paigerube14@users.noreply.github.com>
2022-10-12 03:19:48 -0400

    adding nightly laoded upgrade to new sheet (#295)

--- write_to_sheet/write_loaded_results.py
+++ write_to_sheet/write_loaded_results.py
@@ -37,9 +37,9 @@ def write_to_sheet(google_sheet_account, flexy_id, scale_ci_job, upgrade_job_url
     tz = timezone('EST')
 
     worker_count = write_helper.get_worker_num(scale)
-
+    env_vars = write_helper.get_env_vars_from_file(env_vars_file)
     cloud_type, arch_type, network_type = write_helper.flexy_install_type(flexy_url)
-    row = [flexy_cell, versions[0], cloud_type, arch_type, network_type, worker_count, ci_cell, upgrade_path_cell, status_cell, str(datetime.now(tz)),write_helper.get_env_vars_from_file(env_vars_file)]
+    row = [flexy_cell, versions[0], cloud_type, arch_type, network_type, worker_count, ci_cell, upgrade_path_cell, status_cell, str(datetime.now(tz)),env_vars]
     ws.insert_row(row, index, "USER_ENTERED")
 
     return_code, worker_master = write_helper.run("oc get nodes | grep worker | grep master|  wc -l | xargs")
@@ -51,11 +51,20 @@ def write_to_sheet(google_sheet_account, flexy_id, scale_ci_job, upgrade_job_url
         sno = "yes"
 
     last_version = versions[-1].split(".")
-    row = [flexy_cell, versions[0], upgrade_path_cell, ci_cell, worker_count, status_cell, duration,scale, force,
+    last_version_string = str(last_version[0]) + "." + str(last_version[1])
+    row1 = [flexy_cell, versions[0], upgrade_path_cell, ci_cell, worker_count, status_cell, duration,scale, force,
            cloud_type, install_type, network_type, sno, str(datetime.now(tz)), env_vars]
     upgrade_sheet = file.open_by_url(
         "https://docs.google.com/spreadsheets/d/1yqQxAxLcYEF-VHlQ_KDLs8NOFsRLb4R8V2UM9VFaRBI/edit?usp=sharing")
-    ws_upgrade = upgrade_sheet.worksheet(str(last_version[0]) + "." + str(last_version[1]))
-    ws_upgrade.insert_row(row, index, "USER_ENTERED")
+    ws_upgrade = upgrade_sheet.worksheet(last_version_string)
+    ws_upgrade.insert_row(row1, index, "USER_ENTERED")
 
-#write_to_sheet("/Users/prubenda/.secrets/perf_sheet_service_account.json", "50396", "https://mastern-jenkins-csb-openshift-qe.apps.ocp-c1.prod.psi.redhat.com/job/scale-ci/job/paige-e2e-multibranch/job/cluster-density/126/", "https://mastern-jenkins-csb-openshift-qe.apps.ocp-c1.prod.psi.redhat.com/job/scale-ci/job/paige-e2e-multibranch/job/upgrade_test/180/", "https://mastern-jenkins-csb-openshift-qe.apps.ocp-c1.prod.psi.redhat.com/job/scale-ci/job/paige-e2e-multibranch/job/loaded-upgrade/167/console", "TEST", False, True)
+    cluster_name = write_helper.run('oc get routes -n openshift-console console')[1]
+    print('cluster name ' + str(cluster_name))
+    if "load-up" in cluster_name: 
+        sheet = file.open_by_url("https://docs.google.com/spreadsheets/d/1Zp116e0goej2Q8TOF6o9AwsXbWd-WqCsmwhFA1SFdIk/edit#gid=664841475")
+        ws = sheet.worksheet(last_version_string + " Upgrade")
+        ws.insert_row(row, index, "USER_ENTERED")
+    
+
+#write_to_sheet("/Users/prubenda/.secrets/perf_sheet_service_account.json", "145353", "https://mastern-jenkins-csb-openshift-qe.apps.ocp-c1.prod.psi.redhat.com/job/scale-ci/job/paige-e2e-multibranch/job/kube-burner/1411/", "https://mastern-jenkins-csb-openshift-qe.apps.ocp-c1.prod.psi.redhat.com/job/scale-ci/job/paige-e2e-multibranch/job/upgrade_test/342/", "https://mastern-jenkins-csb-openshift-qe.apps.ocp-c1.prod.psi.redhat.com/job/scale-ci/job/paige-e2e-multibranch/job/loaded-upgrade/774/console", "TEST", False, True,"blank.out")
