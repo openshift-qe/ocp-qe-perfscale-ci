32d06b1f6cd3f9fc1641d5869004909045ec257a
Paige Rubendall <prubenda@redhat.com>
2023-03-03 15:02:54 -0500

    updating update json

--- update_json_uuid.py
+++ update_json_uuid.py
@@ -1,6 +1,26 @@
 import json
 import helper_uuid
 
+from utils import *
+from optparse import OptionParser
+
+cliparser = OptionParser()
+
+cliparser.add_option("-f", "--flexy_template", dest="flexy_template",
+                     help="This is the template type of what built the cluster.",
+                     )
+cliparser.add_option("-w", "--workload", dest="workload",
+                     help="This is the workload type that was run. See kube-burner-job label in created namespace for help")
+cliparser.add_option("-u", "--uuid", dest="uuid",
+                     help="This is the workload type that was run. See kube-burner-job label in created namespace for help")
+
+(options, args) = cliparser.parse_args()
+
+
+globalvars = {}
+globalvars["flexy_template"] = options.flexy_template
+globalvars["workload"] = options.workload
+globalvars["uuid"] = options.uuid
 
 def update_with_current_uuid(uuid, workload, flexy_template ): 
     split_temp =helper_uuid.split_flexy_temp(flexy_template)
@@ -14,12 +34,17 @@ def update_with_current_uuid(uuid, workload, flexy_template ):
     ocp_version = split_temp[0]
     cluster_worker_count = helper_uuid.get_worker_num() 
     cluster_arch_type = helper_uuid.get_arch_type()
-    found_uuid = helper_uuid.find_uuid(read_json, ocp_version, cluster_worker_count, cluster_arch_type)
+    network_type = split_temp[2]
+    found_uuid = helper_uuid.find_uuid(read_json, ocp_version, cluster_worker_count, network_type, cluster_arch_type)
+    
     if found_uuid == "":
-        read_json[ocp_version][cluster_worker_count][cluster_arch_type] = uuid
+        network_type = network_type[-3:].upper()
+        read_json[ocp_version][cluster_worker_count][network_type][cluster_arch_type] = uuid
     
     with open(workload + "/" + platform + ".json", "w") as f:
         f.write(json.dumps(read_json))
 
+    with open(workload + "/update.json", "w") as f:
+        f.write(json.dumps(read_json))
 
-update_with_current_uuid("uuid","cluster-density","../4.12/P1/IPI-on-Azure-fully-private.install.yaml")
+update_with_current_uuid(globalvars["uuid"],globalvars["workload"],globalvars["flexy_template"])
