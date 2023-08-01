a9b4a89e869c2f543d755d88e5973246519e7955
liqcui <liqcui@redhat.com>
2023-08-01 08:10:04 +0000

    update code per pr comments

--- upgrade_scripts/check-rosa-upgrade.sh
+++ upgrade_scripts/check-rosa-upgrade.sh
@@ -2,8 +2,6 @@
 source ./common.sh
 upgrade_pass=True
 
-capture_failed_pods_before_upgrade
-
 SECONDS=0
 export PYTHONUNBUFFERED=1
 python3 -c "import check_upgrade; check_upgrade.check_upgrade('$target_version_prefix')"
@@ -11,6 +9,4 @@ duration=$SECONDS
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
 sleep 120
 abnormal_co
-sleep 120
-capture_failed_pods_after_upgrade
 exit 0 #upgrade succ and post-check succ
