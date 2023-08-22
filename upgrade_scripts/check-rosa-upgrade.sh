934c7ddb761ec77301b9538b244b33cc26c235a3
liqcui <liqcui@redhat.com>
2023-08-22 10:28:42 +0000

    update code to check-rosa-upgrade

--- upgrade_scripts/check-rosa-upgrade.sh
+++ upgrade_scripts/check-rosa-upgrade.sh
@@ -3,7 +3,10 @@ source ./common.sh
 upgrade_pass=True
 
 SECONDS=0
+TARGET_BUILD=${TARGET_BUILD:=""}
 export PYTHONUNBUFFERED=1
+echo TARGET_RELEASES is $TARGET_RELEASES
+target_version_prefix=${TARGET_RELEASES}
 python3 -c "import check_upgrade; check_upgrade.check_upgrade('$target_version_prefix')"
 duration=$SECONDS
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
