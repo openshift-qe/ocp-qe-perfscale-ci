507afb4877d704c5699a805a8deb70e56274d27a
memodi <memodi@redhat.com>
2024-10-29 17:28:00 -0400

    add env.sh

---
+++ scripts/env.sh
@@ -0,0 +1,8 @@
+# Should be 'Official', 'Internal', 'OperatorHub' or 'Source'
+export INSTALLATION_SOURCE=Internal
+# will use 'Released' if not set otherwise
+export LOKI_OPERATOR=Unreleased
+# will use '1x.extra-small' if not set 
+export LOKISTACK_SIZE=1x.small
+# will use '6' if not set 
+export TOPIC_PARTITIONS=48
