8b6ce2d38c98390db18545a3d3c5243caf2d216c
Mehul Modi <memodi@redhat.com>
2024-11-14 11:36:01 -0500

    NETOBSERV-1874 Netobserv reliability tests (#659)
    * add env.sh
    
    * default vars
    
    * set ebpf to privileged mode by default
    
    * file descriptors and goroutines panels

---
+++ scripts/env.sh
@@ -0,0 +1,6 @@
+# will use 'Released' if not set otherwise
+export LOKI_OPERATOR=Unreleased
+# will use '1x.extra-small' if not set 
+export LOKISTACK_SIZE=1x.extra-small
+# will use '6' if not set 
+export TOPIC_PARTITIONS=48
